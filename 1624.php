e38b1c9ecbdb8392d4d5882b917f00ac4ae0a800
11ze <wangzecn@qq.com>
2022-09-18 15:42:48 +0800

    feat: add 1624

---
+++ 1624.php
@@ -0,0 +1,39 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * 1624. 两个相同字符之间的最长子字符串
+     * https://leetcode.cn/problems/largest-substring-between-two-equal-characters/
+     * @param String $s
+     * @return Integer
+     */
+    public function maxLengthBetweenEqualCharacters($s)
+    {
+        $saved = []; // [key => [max, index]];
+        $max = -1;
+
+        foreach (str_split($s) as $index => $char) {
+            $savedItem = $saved[$char] ?? null;
+
+            if (is_null(($savedItem))) {
+                $saved[$char] = [
+                    'max' => -1,
+                    'index' => $index,
+                ];
+                continue;
+            }
+
+            $size = $index - $savedItem['index'] - 1;
+
+            if ($savedItem['max'] < $size) {
+                $savedItem['max'] = $size;
+                $max = $max < $size ? $size : $max;
+            }
+            $savedItem['index'] = $index;
+        }
+
+        return $max;
+    }
+}
